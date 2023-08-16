e1e6a1778f0eeafa931ee396877a4bbe8e17cbf5
Raxmonov Ozodbek <rahmonov.ozodbek.99@gmail.com>
2023-08-16 18:41:11 +0500

    Create lesson2.py

---
+++ lesson2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 10 09:56:07 2023
+
+@author: Raxmonov
+"""
+python_codes = {
+    'int' : 'Integer - butun sonlarni ifodalaydi',
+    'float' : 'sonlarni ifodalaydi',
+    'len' : 'Berilgan elementning uzunligini aniqlaydi',
+    'elif' : 'Shart operatorida 2 tadan ortiq tekshiruv bo`lganida bajariladi',
+    'tuple' : 'Tuple - Ro`yxatdagi ma\'lumotlarni o`zgarmas tipga o`tkazadi',
+    'list' : 'Berilgan ma\'lumotlarni ro`yxat ko\'rinishiga keltiradi',
+    'range' : 'berilgan parametrlar bo`yicha raqamli qatorni hosil qilib beradi',
+    'in' : 'for operatorida bog`lovchi vazifasini bajaradi, boshqa joylarda esa tekshiruv vazifasini bajaradi',
+    'min' : 'Berilgan ro`yxatdagi eng kichik raqamni yoki matnni aniqlaydi',
+    'sum' : 'Berilgan massivdagi sonlar yig`indisini qaytaradi'
+}
+
+print(python.get('len', 'Ma\'lumot topilmadi'))
