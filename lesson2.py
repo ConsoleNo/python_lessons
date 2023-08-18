8ae94616731509b6168ea44aa807ef7980416c3c
Raxmonov Ozodbek <rahmonov.ozodbek.99@gmail.com>
2023-08-18 20:48:23 +0500

    Update lesson2.py

--- lesson2.py
+++ lesson2.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Aug 10 09:56:07 2023
-
-@author: Raxmonov
-"""
 python_codes = {
     'int' : 'Integer - butun sonlarni ifodalaydi',
     'float' : 'sonlarni ifodalaydi',
@@ -17,4 +12,51 @@ python_codes = {
     'sum' : 'Berilgan massivdagi sonlar yig`indisini qaytaradi'
 }
 
-print(python_codes.get('len', 'Ma\'lumot topilmadi'))
+kompyuterlar = { 
+    "acer" : 7_500_000,
+    "toshiba" : 5_500_000,
+    "vp" : 3_500_000,
+    "lenovo" : 9_500_000,
+    "legion" : 7_500_000,
+    "hp" : 5_500_000,
+    "hp1" : 5_500_000,
+    "hp2" : 5_500_000,
+    "hp3" : 5_500_000,
+    "hp4" : 5_500_000,
+    "hp5" : 5_500_000,
+}
+
+countr = {
+    'Uzbekistan': 'Tashkent',
+    'Russia': 'Moscow',
+    'Pakistan': 'Islamabad',
+    'Armenia': 'Erevan'
+}
+
+restoran = {
+    'shashlik': 50000,
+    'mastava': 20200,
+    'sho`rva': 45000,
+    'choy': 5000,
+    'lag`mon': 40000,
+    'limon': 45000
+}
+
+m1 = input('1-taomni kiriting: ').lower()
+m2 = input('2-taomni kiriting: ').lower()
+m3 = input('3-taomni kiriting: ').lower()
+
+if m1 in restoran: 
+    print(f"{m1} - {restoran[m1]}")
+else: 
+    print(f"{m1} - Bizda bunday taom yo`q")
+
+if m2 in restoran: 
+    print(f"{m2} - {restoran[m2]}")
+else: 
+    print(f"{m2} - Bizda bunday taom yo`q")
+
+if m3 in restoran: 
+    print(f"{m3} - {restoran[m3]}")
+else: 
+    print(f"{m3} - Bizda bunday taom yo`q")
